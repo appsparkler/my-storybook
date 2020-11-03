58a3e2ba336cb23707843feff4175000a287a3d6
Aakash <subs@appsparkler.com>
2020-11-03 12:29:47 +0530

    Setting up default values for date and time

--- src/components/DateTimeForm.js
+++ src/components/DateTimeForm.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Stack, DatePicker, MaskedTextField}from '@fluentui/react'
+import moment from 'moment'
 
-const DateTimeForm = () => (
+const DateTimeForm = ({
+  timeField,
+  dateField
+}) => (
   <Stack horizontal tokens={{childrenGap: 10}}>
     <Stack.Item>
-      <DatePicker label="Date" />
+      <DatePicker
+        label="Date"
+        {...dateField}
+      />
     </Stack.Item>
     <Stack.Item>
       <MaskedTextField
         label="Time"
         mask="99:99"
+        {...timeField}
       />
     </Stack.Item>
   </Stack>
 )
+
+DateTimeForm.propTypes = {
+  timeField: PropTypes.object,
+  dateField: PropTypes.object
+}
+
+DateTimeForm.defaultProps = {
+  timeField: {
+    value: moment().format('HH:mm')
+  },
+  dateField: {
+    value: new Date()
+  },
+}
 
 export default React.memo(DateTimeForm)
