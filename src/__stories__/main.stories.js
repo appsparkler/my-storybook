d1569793aacb8ba3a574848659bbca52209afde1
Aakash <subs@appsparkler.com>
2020-11-09 18:02:20 +0530

    Setting up Pivot

--- src/__stories__/main.stories.js
+++ src/__stories__/main.stories.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Pivot, PivotItem, Label,Stack } from '@fluentui/react'
+import {Pivot, PivotItem, Label,Stack, Text } from '@fluentui/react'
 
 export default {
   title: 'Test'
@@ -13,19 +13,23 @@ export const PivotTest = () => {
     <Stack tokens={{padding: 10}}>
     <Pivot aria-label="Basic Pivot Example" className="ms-depth-8">
       <PivotItem
-        headerText="My Files"
+        headerText="Convert 2 Timestamp"
         headerButtonProps={{
           'data-order': 1,
           'data-title': 'My Files Title',
         }}
       >
-        <Label styles={labelStyles}>Pivot #1</Label>
-      </PivotItem>
-      <PivotItem headerText="Recent">
-        <Label styles={labelStyles}>Pivot #2</Label>
-      </PivotItem>
-      <PivotItem headerText="Shared with me">
         <Stack tokens={{padding: 10}}>
+          <Text variant="xLarge">
+            Convert 2 Timestamp
+          </Text>
+        </Stack>
+      </PivotItem>
+      <PivotItem headerText="Convert 2 Date">
+        <Stack tokens={{padding: 10}}>
+          <Label variant="xLarge">
+            Convert 2 Date
+          </Label>
           <Label styles={labelStyles}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Label>
         </Stack>
       </PivotItem>
