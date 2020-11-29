003ea994e06a1a65b754510472649da99fe745b7
Aakash <subs@appsparkler.com>
2020-11-29 07:27:42 +0530

    ScheduledSlotForm - WIP

--- src/apps/PunchCard/ScheduledSlotForm/index.js
+++ src/apps/PunchCard/ScheduledSlotForm/index.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import {CustomLabelVariantA} from '../../../components/CustomLabel/variantA'
 import {
   MaskedTextField, PrimaryButton,
-  Stack
+  Stack, mergeStyleSets
 } from '@fluentui/react'
+import moment from 'moment'
 
 const ScheduledSlotFormLayout = ({
   form,
@@ -29,28 +30,30 @@ const ScheduledSlotFormLayout = ({
 export const ScheduledSlotForm = ({
   onSubmit
 }) => {
+  const classNames = React.useMemo(() => mergeStyleSets({
+    textField: {
+      maxWidth: 130
+    }
+  }),[])
 
-  const scheduledSlotForm =  {
-    form: {
-      onSubmit: React.useCallback((evt) => {
-        evt.preventDefault();
-        evt.stopPropagation();
-        onSubmit(evt)
-      },[onSubmit])
-    },
+  const [state, setState] = React.useState({
     maskedTextField0: {
+      className: classNames.textField,
       label:"Start Time *",
-      content:"In MM-DD-YYYY HH:mm format...",
+      content:"In YYYY-MM-DD HH:mm format...",
       required: true,
-      mask:"99-99-9999 99:99",
+      mask:"9999-99-99 99:99",
       text:"Hello",
-      onRenderLabel:(props) => <CustomLabelVariantA {...props} />
+      onRenderLabel:(props) => <CustomLabelVariantA {...props} />,
+      value: ''
     },
     maskedTextField1: {
+      className: classNames.textField,
       label:"End Time *",
-      content:"In MM-DD-YYYY HH:mm format...",
+      content:"In YYYY-MM-DD HH:mm format...",
       required: true,
-      mask:"99-99-9999 99:99",
+      mask:"9999-99-99 99:99",
+      value: '',
       onRenderLabel: (props) =>  <CustomLabelVariantA {...props} />
     },
     primaryButton0: {
@@ -59,6 +62,79 @@ export const ScheduledSlotForm = ({
       iconProps:{
         iconName: 'Add'
       }
+    }
+  });
+
+  const updateMaskedTextField0Value = React.useCallback((evt, value) => {
+    setState(currentState => ({
+      ...currentState,
+      maskedTextField0: {
+        ...currentState.maskedTextField0,
+        value
+      },
+    }))
+  },[])
+
+  const updateMaskedTextField1Value = React.useCallback((evt, value) => {
+    setState(currentState => ({
+      ...currentState,
+      maskedTextField1: {
+        ...currentState.maskedTextField1,
+        value
+      },
+    }))
+  },[])
+
+  const resetFields = React.useCallback(() => {
+    setState(currentState => ({
+      ...currentState,
+      maskedTextField0: {
+        ...currentState.maskedTextField0,
+        value: ''
+      },
+      maskedTextField1: {
+        ...currentState.maskedTextField1,
+        value: ''
+      },
+    }))
+  },[])
+
+  const scheduledSlotForm =  {
+    form: {
+      onSubmit: React.useCallback((evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+        const format = 'YYYY-MM-DD HH:mm'
+        const startTime = state.maskedTextField0.value;
+        const endTime = state.maskedTextField1.value;
+        const startTimeMoment = moment(startTime, format);
+        const endTimeMoment = moment(endTime, format);
+        const isEndTimeGreaterThanStartTime = endTimeMoment > startTimeMoment;
+        const momentsAreValid = startTimeMoment.isValid() && endTimeMoment.isValid();
+        const ok2Submit = isEndTimeGreaterThanStartTime && momentsAreValid;
+        if(ok2Submit) {
+          onSubmit({
+            startTime: startTimeMoment.valueOf(),
+            endTime: endTimeMoment.valueOf()
+          })
+          resetFields()
+        }
+      },[
+        onSubmit, resetFields,
+        state.maskedTextField1.value,
+        state.maskedTextField0.value,
+      ])
+    },
+    maskedTextField0: {
+      ...state.maskedTextField0,
+      onChange: updateMaskedTextField0Value
+    },
+    maskedTextField1: {
+      ...state.maskedTextField1,
+      onChange: updateMaskedTextField1Value
+    },
+    primaryButton0: {
+      ...state.primaryButton0
     }
   }
 
