39e774da521b63088cee4d29536a8a019404d61c
Aakash <subs@appsparkler.com>
2020-12-03 13:56:47 +0530

    ScheduledSlots - WIP

--- src/apps/PunchCardApp/PunchCard/ScheduledSlots/__stories__/main.stories.js
+++ src/apps/PunchCardApp/PunchCard/ScheduledSlots/__stories__/main.stories.js
@@ -8,7 +8,15 @@ const Story = {
 
 export const Template = (args) => <ScheduledSlots {...args} />
 Template.args = {
-
+  items: [{
+      id: 'ae084dcd-d3c6-4749-8815-bc505987aaf3',
+      inTime: 1606887000000,
+      outTime: 1606890600000
+    },{
+    id: 'd47fcf33-bf96-4f16-8158-b21d78017300',
+    inTime: 1606894200000,
+    outTime: 1606897800000
+  }]
 }
 
 export default Story
--- src/apps/PunchCardApp/PunchCard/ScheduledSlots/index.js
+++ src/apps/PunchCardApp/PunchCard/ScheduledSlots/index.js
@@ -1,10 +1,13 @@
 import React  from 'react'
+import PropTypes from 'prop-types'
 import {
   IconButton, SelectionMode,
   mergeStyleSets
 } from '@fluentui/react'
 import DetailsListWithText from '../../../../components/DetailsListWithText'
 import DateTimeTextField from '../PunchInTimeCell'
+import {FORMAT} from '../../shared'
+import moment from 'moment'
 
 const InTextField = ({inTextField}) => <DateTimeTextField {...inTextField}/>
 
@@ -45,57 +48,118 @@ const classNames = mergeStyleSets({
   },
 })
 
-const ScheduledSlots = () => {
+const ScheduledSlots = ({
+  items, onDeleteSlot, onChangeSlot
+}) => {
+  const [state, setState] = React.useState({
+    detailsList: {
+      items: []
+    }
+  })
+
+  const updateDetailsList = React.useCallback((update) => {
+    setState(currenState => ({
+      ...currenState,
+      detailsList: {
+        ...currenState.detailsList,
+        ...update
+      }
+    }))
+  },[])
+
   const detailsListWithText = {
     show: true,
     detailsList: {
+      ...state.detailsList,
       className: classNames.detailsList,
       selectionMode: SelectionMode.none,
       columns: [{
+        key: 'inTime',
         name: 'In Time',
         onRender: InTextField
       }, {
+        key: 'outTime',
         name: 'Out Time',
         onRender: OutTextField
       },{
         key: "deleteicon",
         className: 'deleteIconColumn',
-        onRender: () => <IconButton
+        onRender: ({deleteIconButton}) => <IconButton
           iconProps={{iconName: 'Trash'}}
+          {...deleteIconButton}
         />
       }],
-      items: [{
-        id: '123',
-        name: 'In Time',
-        inTextField: {
-          value: '2020-11-12 12:30',
-          onError: () => alert('oops'),
-          onChange: () => alert('ok')
-        },
-        outTextField: {
-          value: '2020-11-12 13:30',
-          onError: () => alert('oops out'),
-          onChange: () => alert('ok out')
-        },
-      },{
-        id: '124',
-        inTextField: {
-          value: '2020-11-12 11:30',
-          onError: () => alert('oops'),
-          onChange: () => alert('ok')
-        },
-        outTextField: {
-          value: '2020-11-12 12:30',
-          onError: () => alert('oops'),
-          onChange: () => alert('ok')
-        }
-      }]
     },
     text: {
 
     }
   }
+
+  React.useEffect(() => {
+    // const updatedItems = [{
+    //   id: '123',
+    //   key: '123',
+    //   name: 'In Time',
+    //   inTextField: {
+    //     value: '2020-11-12 12:30',
+    //     onError: () => alert('oops'),
+    //     onChange: () => alert('ok')
+    //   },
+    //   outTextField: {
+    //     value: '2020-11-12 13:30',
+    //     onError: () => alert('oops out'),
+    //     onChange: () => alert('ok out')
+    //   },
+    //   deleteIconButton: {
+    //     onClick: () => alert('lets delete 123')
+    //   }
+    // },{
+    //   id: '124',
+    //   key: '124',
+    //   inTextField: {
+    //     value: '2020-11-12 11:30',
+    //     onError: () => alert('oops'),
+    //     onChange: () => alert('ok')
+    //   },
+    //   outTextField: {
+    //     value: '2020-11-12 12:30',
+    //     onError: () => alert('oops'),
+    //     onChange: () => alert('ok')
+    //   },
+    //   deleteIconButton: {
+    //     onClick: () => alert('lets delete 124')
+    //   }
+    // }]
+    const updatedItems = items
+      .map(item => ({
+        id: item.id,
+        inTextField: {
+          value: moment(item.inTime).format(FORMAT),
+          onError: () => alert('oops out'),
+          onChange: () => alert('ok out')
+        },
+        outTextField: {
+          value: moment(item.outTime).format(FORMAT),
+          onError: () => alert('oops out'),
+          onChange: () => alert('ok out')
+        },
+        deleteIconButton: {
+          onClick: () => alert(`ready to delete ${item.id}?`)
+        }
+      }))
+
+    updateDetailsList({
+      items: updatedItems
+    })
+  },[updateDetailsList, items])
   return <DetailsListWithText {...detailsListWithText} />
+}
+
+ScheduledSlots.propTypes = {
+  items: PropTypes.array,
+
+  onDeleteSlot: PropTypes.func,
+  onChangeSlot: PropTypes.func
 }
 
 
