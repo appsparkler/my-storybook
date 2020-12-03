3babf231fa228192e386f59230f9a043a58f619a
Aakash <subs@appsparkler.com>
2020-12-03 08:50:45 +0530

    Added story for Spinner

---
+++ src/apps/PunchCardApp/PunchCard/Spinner/__stories__/main.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Spinner from '../'
+
+const Story = {
+  title: 'Apps/Punch Card App/Punch Card/Spinner',
+  component: Spinner
+}
+
+export const Template = (args) => <Spinner {...args}/>
+Template.args = {
+  show: true
+}
+Template.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
+
+export default Story
