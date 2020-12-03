234f433c801c00bf74698225068b22c9be06fdd9
Aakash <subs@appsparkler.com>
2020-12-03 10:43:42 +0530

    Scheduled Slots - WIP

---
+++ src/apps/PunchCardApp/PunchCard/ScheduledSlots/__stories__/main.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ScheduledSlots from '..'
+
+const Story = {
+  title: 'Apps/Punch Card App/Punch Card/Scheduled Slots',
+  component: ScheduledSlots
+}
+
+export const Template = (args) => <ScheduledSlots {...args} />
+Template.args = {
+
+}
+
+export default Story
--- src/apps/PunchCardApp/PunchCard/ScheduledSlots/index.js
+++ src/apps/PunchCardApp/PunchCard/ScheduledSlots/index.js
@@ -1,9 +1,77 @@
 import React  from 'react'
-import DetailsListWithText from '../../'
+import {
+  IconButton, SelectionMode,
+  mergeStyleSets
+} from '@fluentui/react'
+import DetailsListWithText from '../../../../components/DetailsListWithText'
+import DateTimeTextField from '../PunchInTimeCell'
 
-const ScheduledSlots = ({
-  detailsListWithText
-}) => <DetailsListWithText {...detailsListWithText} />
+const InTextField = ({inTextField}) => <DateTimeTextField {...inTextField}/>
+
+const OutTextField = ({outTextField}) => <DateTimeTextField {...outTextField}/>
+
+const classNames = mergeStyleSets({
+  detailsList: {
+    '.ms-DetailsList-headerWrapper': {
+      '.ms-FocusZone.ms-DetailsHeader': {
+        paddingTop: 0,
+        '.ms-DetailsHeader-cell': {
+          maxWidth: '40%',
+          minWidth: '40%'
+        }
+      }
+    },
+    '.ms-DetailsRow-fields': {
+      display: 'flex',
+      '.ms-DetailsRow-cell': {
+        maxWidth: '40%',
+        minWidth: '40%'
+      }
+    }
+  },
+})
+
+const ScheduledSlots = () => {
+  const detailsListWithText = {
+    show: true,
+    detailsList: {
+      className: classNames.detailsList,
+      selectionMode: SelectionMode.none,
+      columns: [{
+        name: 'In Time',
+        onRender: InTextField
+      }, {
+        name: 'Out Time',
+        onRender: OutTextField
+      },{
+        name: '',
+        onRender: () => <IconButton
+          iconProps={{iconName: 'Trash'}}
+        />
+      }],
+      items: [{
+        id: '123',
+        name: 'In Time',
+        inTextField: {
+          value: '2020-11-12 12:30'
+        },
+        outTextField: {
+          value: '2020-11-12 13:30'
+        },
+      },{
+        id: '124',
+        inTextField: {
+          value: '2020-11-12 11:30'
+        },
+        outTextField: {
+          value: '2020-11-12 12:30'
+        }
+      }]
+    },
+    text: {}
+  }
+  return <DetailsListWithText {...detailsListWithText} />
+}
 
 
 export default React.memo(ScheduledSlots)
