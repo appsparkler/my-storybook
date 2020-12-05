ae8e4555346d190da55f3d91c6f0cf262c260a18
Aakash <subs@appsparkler.com>
2020-12-05 12:51:51 +0530

    Adding punch card in DB on ADd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import PunchCardApp from './apps/PunchCardApp'
+import db from './db'
+import {v4 as uuid} from 'uuid'
 
 const App = () => {
-  const  punchCardApp = {
-    isPunchCardsPanelOpen: false,
-    punchCards: [],
-    selectedPunchCard: null,
-    onAddPunchCard: React.useCallback(() => {
+  const [state, setState] = React.useState({
+    punchCards: []
+  })
 
+  const  punchCardApp = {
+    ...state,
+    isPunchCardsPanelOpen: false,
+    selectedPunchCard: null,
+    onAddPunchCard: React.useCallback(async(title) => {
+      const newPunchCard = {
+        id: uuid(),
+        title,
+        punchedSlots: [],
+        scheduledSlots: [],
+        goalForTheDay: {
+          hours: '00',
+          minutes: '09'
+        }
+      };
+      await db.punchCards.add(newPunchCard)
+      setState(currentState => ({
+        punchCards: [
+          newPunchCard,
+          ...currentState.punchCards,
+        ]
+      }))
     },[]),
     onDismissPunchCardPanel: React.useCallback(() => {
 
