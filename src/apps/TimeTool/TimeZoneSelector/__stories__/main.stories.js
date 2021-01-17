eef3775d8c21eb39fc3412382ceef526e30b40e9
Aakash <subs@appsparkler.com>
2021-01-17 11:26:11 +0530

    Callout - WIP

--- src/apps/TimeTool/TimeZoneSelector/__stories__/main.stories.js
+++ src/apps/TimeTool/TimeZoneSelector/__stories__/main.stories.js
@@ -1,27 +1,57 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {TextField} from '@fluentui/react/lib/TextField'
-import {Callout} from '@fluentui/react/lib/Callout'
-import {Stack} from '@fluentui/react/lib/Stack'
+// import PropTypes from 'prop-types'
+import {
+  mergeStyleSets, TextField,
+  Callout,Stack
+} from '@fluentui/react'
+
+const styles = mergeStyleSets({
+  callout: {
+    maxWidth: 300,
+    minWidth: 300
+  }
+})
 
 const TimeZoneSelector = () => {
+  const [state, setState] = React.useState({
+    showCallout: false
+  })
   const textField = {
     placeholder: 'Select Timezone',
     className: '.test'
   }
+
+  const onClick = React.useCallback(() => {
+    setState(currentState => ({
+      ...currentState,
+      showCallout: true
+    }))
+  },[])
+
+  const onDismiss = React.useCallback(() => {
+    setState(currentState => ({
+      ...currentState,
+      showCallout: false
+    }))
+  },[])
   return (
-    <Stack horizontal>
-      <TextField {...textField} />
-      <Callout
-        onDismiss={() => alert('dismissed')}
-        target=".test"
-        coverTarget
-        isBeakVisible={false}
-        gapSpace={0}
-        setInitialFocus
-      >
-          <h1>TimeZone Callout</h1>
+    <Stack vertical>
+      <button onClick={onClick}>Show Callout</button>
+      <TextField {...textField} className="test" />
+      {state.showCallout && (
+        <Callout
+          className={styles.callout}
+          onDismiss={onDismiss}
+          target=".test"
+          isBeakVisible
+          gapSpace={0}
+          setInitialFocus
+        >
+            <h1>TimeZone Callout</h1>
         </Callout>
+      )}
+
+      <pre>{JSON.stringify({state}, null, 2)}</pre>
     </Stack>
   )
 }
