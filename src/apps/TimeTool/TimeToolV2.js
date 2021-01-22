dc07dfd74f33d9b30a2b379558352b577befeec3
Aakash <subs@appsparkler.com>
2021-01-22 11:10:08 +0530

    Ticking digital clock

--- src/apps/TimeTool/TimeToolV2.js
+++ src/apps/TimeTool/TimeToolV2.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import {Stack} from '@fluentui/react'
+import moment from 'moment'
 import DigitalClock from './DigitalClock/index.jsx'
 import TimeZoneSelector from './TimeZoneSelector/index.jsx'
 
 const TimeTool = () => {
-  const digitalClock = {
-    timestamp: Date.now(),
-  }
+  const [{selectedTimezone, now}, setState] = React.useState({
+    selectedTimezone: moment.tz.guess(),
+    now: Date.now()
+  })
+
+  const digitalClock = React.useMemo(() => {
+    return {
+      timestamp: moment(
+        moment(now)
+          .tz(selectedTimezone)
+          .toArray()
+      ).valueOf(),
+    }
+  },[selectedTimezone, now])
 
   const timezoneSelector = {
     onSelectTimezone: React.useCallback((tz) => {
-      
+      setState(currentState => ({
+        ...currentState,
+        selectedTimezone: tz.name
+      }))
     }, [])
   }
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setState(currentState => ({
+        ...currentState,
+        now: Date.now()
+      }))
+    }, 1000)
+  }, [])
+
   return (
     <Stack>
       <DigitalClock {...digitalClock} />
@@ -21,5 +46,4 @@ const TimeTool = () => {
   )
 };
 
-
-export default TimeTool;
+export default React.memo(TimeTool);
