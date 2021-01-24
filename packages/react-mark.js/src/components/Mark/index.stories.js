6621225649b9a8379137b85e8b135e5adefb382e
Aakash <subs@appsparkler.com>
2021-01-24 15:47:54 +0530

    Re-organized

---
+++ packages/react-mark.js/src/components/Mark/index.stories.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Mark from '.'
+
+const Story = {
+  title: 'components/Mark',
+  component: Mark
+}
+
+const Template = (args) => (
+  <Mark {...args}>
+    {args.children}
+  </Mark>
+)
+Template.args = {
+  children: 'Hello World',
+  mark: 'o',
+}
+
+export const String = Template.bind({});
+String.args = { ...Template.args }
+
+export const RegExp = Template.bind({});
+RegExp.args = {
+  ...Template.args,
+  type: 'markRegExp',
+  mark: /l/
+}
+
+export const Ranges = Template.bind({});
+Ranges.args = {
+  ...Template.args,
+  type: 'markRanges',
+  mark: [{
+    start: 1,
+    length: 3
+  },{
+    start: 6,
+    length: 4
+  }],
+}
+
+export default Story
--- packages/react-mark.js/src/index.js
+++ packages/react-mark.js/src/index.js
@@ -1,56 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import MarkJS from 'mark.js/dist/mark.es6.min'
+import Mark from './components/Mark'
 
-const unmark = (markJsInstance) => new Promise((done) => {
-  markJsInstance.unmark({
-    done
-  })
-})
-
-const Mark = (props) => {
-  const markerRef = React.useRef();
-
-  const [markerState, setMarkerState] = React.useState({
-    markJsInstance: null,
-    lastUpdated: null
-  })
-
-  React.useEffect(() => {
-    if(markerRef.current) {
-      setMarkerState(currentState => ({
-        ...currentState,
-        markJsInstance: new MarkJS(markerRef.current)
-      }))
-    }
-  },[])
-
-  React.useEffect(() => {
-    if(markerState.markJsInstance) {
-      unmark(markerState.markJsInstance)
-        .then(() => {
-          markerState
-            .markJsInstance
-            .mark(props.mark, props.options);
-        })
-    }
-  }, [props.mark, markerState.markJsInstance, props.options])
-
-  return (
-    <div ref={markerRef}>
-      {props.children}
-    </div>
-  )
+export {
+  Mark
 }
-
-Mark.propTypes = {
-  /** mark options as documents [here](https://markjs.io/#parameters)*/
-  options: PropTypes.object,
-  mark: PropTypes.string
-}
-
-Mark.defaultProps = {
-  options: {}
-}
-
-export default React.memo(Mark)
