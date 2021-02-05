7bfc0ef888e7c7fb16504a5cff94f75dba4c077e
Aakash <subs@appsparkler.com>
2021-02-05 14:54:52 +0530

    working compilers, minor changes to react-mark compiler

--- packages/react-mark.js/dist/index.js
+++ packages/react-mark.js/dist/index.js
@@ -1,17 +1,15 @@
-'use strict'
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
-})
-Object.defineProperty(exports, 'Mark', {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Mark", {
   enumerable: true,
   get: function get() {
-    return _Mark['default']
-  },
-})
+    return _Mark["default"];
+  }
+});
 
-var _Mark = _interopRequireDefault(require('./components/Mark'))
+var _Mark = _interopRequireDefault(require("./components/Mark"));
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj }
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
