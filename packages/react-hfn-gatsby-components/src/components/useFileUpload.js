2dc0c6f862ebf54c1c5a1af9031dc489b0841fb0
Aakash <subs@appsparkler.com>
2021-02-06 10:50:13 +0530

    Filtering out duplicate files

--- packages/react-hfn-gatsby-components/src/components/useFileUpload.js
+++ packages/react-hfn-gatsby-components/src/components/useFileUpload.js
@@ -13,42 +13,70 @@ const useFileUpload = ({ path }) => {
   const onFilesDrop = React.useCallback(
     (files) => {
       try {
+        const filteredFiles = (() => {
+          if (!uploadedFiles) return files
+          const uploadedFilesArray = Object.entries(uploadedFiles).map(
+            ([key, val]) => val
+          )
+          return files.filter(
+            (file) =>
+              !uploadedFilesArray.some((file1) => file1.name === file.name)
+          )
+        })()
         setState((currentState) => ({
           ...currentState,
           isUploading: true,
         }))
         firebase
-          .uploadFiles(path, files, path)
+          .uploadFiles(path, filteredFiles, path)
           .then((res) => {
             setState((currentState) => ({
               ...currentState,
               isUploading: false,
             }))
           })
-          .catch(console.error)
-          .finally(console.log)
-      } catch (e) {
-        console.log({ e })
+          .catch((err) => {
+            console.error({ err })
+          })
+          .finally(() => {
+            console.log('finally uploaded!')
+          })
+      } catch (err) {
+        console.log({ caughtError: err })
       } finally {
         console.log('DONE')
       }
     },
-    [firebase, path]
+    [firebase, path, uploadedFiles]
   )
+  const resetIsDeleting = () =>
+    setState((currentState) => ({
+      ...currentState,
+      isDeleting: false,
+    }))
   const onFileDelete = React.useCallback(
     (file, key) => {
-      setState((currentState) => ({
-        ...currentState,
-        isDeleting: true,
-      }))
-      return firebase
-        .deleteFile(file.fullPath, `${path}/${key}`)
-        .then((res) => {
-          setState((currentState) => ({
-            ...currentState,
-            isDeleting: false,
-          }))
-        })
+      try {
+        setState((currentState) => ({
+          ...currentState,
+          isDeleting: true,
+        }))
+        return firebase
+          .deleteFile(file.fullPath, `${path}/${key}`)
+          .then((res) => {
+            resetIsDeleting()
+          })
+          .catch((err) => {
+            console.error({ err })
+          })
+          .finally(() => {
+            console.log('finally deleted!')
+          })
+      } catch (e) {
+        console.error({ caughtError: e })
+      } finally {
+        resetIsDeleting()
+      }
     },
     [firebase, path]
   )
