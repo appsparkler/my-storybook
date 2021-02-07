b2486a55bb389863ed968b80809f52f588b94ddf
Aakash <subs@appsparkler.com>
2021-02-07 06:58:47 +0530

    Updated PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,38 +1,38 @@
 module.exports = {
   apps: [
     // react-mark.js Storybook
-    {
-      script: 'yarn workspace react-mark.js storybook',
-      name: 'my-react-markjs-sb',
-      env: {
-        NODE_ENV: 'development',
-      },
-    },
+    // {
+    //   script: 'yarn workspace react-mark.js storybook',
+    //   name: 'my-react-markjs-sb',
+    //   env: {
+    //     NODE_ENV: 'development',
+    //   },
+    // },
 
     // My App
-    {
-      script: 'yarn workspace my-storybook start',
-      name: 'my-app',
-    },
+    // {
+    //   script: 'yarn workspace my-storybook start',
+    //   name: 'my-app',
+    // },
 
     // My Storybook
     {
-      script: 'yarn workspace my-storybook storybook',
-      name: 'my-sb',
+      script: 'yarn --cwd packages/my-storybook storybook',
+      name: 'my-sb : 2000',
     },
 
     // HFN Gatsby Storybook
-    {
-      script: 'yarn workspace react-hfn-gatsby-components storybook',
-      name: 'hfn-gatsby',
-    },
+    // {
+    //   script: 'yarn workspace react-hfn-gatsby-components storybook',
+    //   name: 'hfn-gatsby',
+    // },
     // React HFN Components Storybook
-    {
-      script: 'yarn workspace @appsparkler/react-hfn-components storybook',
-      name: 'react-hfn-components',
-      env: {
-        NODE_ENV: 'development',
-      },
-    },
+    // {
+    //   script: 'yarn workspace @appsparkler/react-hfn-components storybook',
+    //   name: 'react-hfn-components',
+    //   env: {
+    //     NODE_ENV: 'development',
+    //   },
+    // },
   ],
 }
