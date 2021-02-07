004be435ebd217f485867efa4e2956e6f94c1999
Aakash <subs@appsparkler.com>
2021-02-07 12:42:22 +0530

    ignoring dist

--- packages/react-mark.js/dist/index.js
+++ packages/react-mark.js/dist/index.js
@@ -3,13 +3,21 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Mark", {
+Object.defineProperty(exports, "RegExpMarker", {
   enumerable: true,
   get: function get() {
-    return _Mark["default"];
+    return _RegExpMarker["default"];
+  }
+});
+Object.defineProperty(exports, "Marker", {
+  enumerable: true,
+  get: function get() {
+    return _Marker["default"];
   }
 });
 
-var _Mark = _interopRequireDefault(require("./components/Mark"));
+var _RegExpMarker = _interopRequireDefault(require("./components/RegExpMarker"));
+
+var _Marker = _interopRequireDefault(require("./components/Marker"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
