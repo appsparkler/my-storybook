3622b479be9ecb695d4e9d51870811a01c4f655c
Aakash <subs@appsparkler.com>
2021-02-07 12:15:07 +0530

    Refined Regexp marker

--- packages/react-mark.js/src/components/RegExMarker/index.js
+++ packages/react-mark.js/src/components/RegExMarker/index.js
@@ -1,45 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import MarkJS from 'mark.js/dist/mark.es6.min'
-
-const unmark = (markJsInstance) =>
-  new Promise((done) => {
-    markJsInstance.unmark({
-      done,
-    })
-  })
-
-const useMarker = ({
-  mark = new RegExp(),
-  options = {},
-  unmarkOptions = {},
-  type = 'mark',
-}) => {
-  const markerRef = React.useRef()
-
-  const [markerState, setMarkerState] = React.useState({
-    markJsInstance: null,
-  })
-
-  React.useEffect(() => {
-    if (markerRef.current) {
-      setMarkerState((currentState) => ({
-        ...currentState,
-        markJsInstance: new MarkJS(markerRef.current),
-      }))
-    }
-  }, [])
-
-  React.useEffect(() => {
-    if (markerState.markJsInstance) {
-      unmark(markerState.markJsInstance, unmarkOptions).then((...args) => {
-        markerState.markJsInstance[type](mark, options)
-      })
-    }
-  }, [mark, options, markerState.markJsInstance, type, unmarkOptions])
-
-  return { markerRef }
-}
+import useMarker from '../useMarker'
 
 const RegExpMarker = ({ children, mark, options, unmarkOptions }) => {
   const { markerRef } = useMarker({
@@ -55,16 +16,18 @@ RegExpMarker.propTypes = {
   /** mark options as documented [in the options section](https://markjs.io/#markregexp)*/
   options: PropTypes.object,
   /** A Regular Expression - for ex `/\.js/g` */
-  mark: PropTypes.instanceOf(RegExp),
+  mark: PropTypes.instanceOf(RegExp).isRequired,
   /** un-mark options as documented [in the options section](https://markjs.io/#unmark)*/
   unmarkOptions: PropTypes.object,
-  children: PropTypes.any,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
 }
 
 RegExpMarker.defaultProps = {
   /** For a full list of options; visit **[markregexp](https://markjs.io/#markregexp)** */
   options: {},
-  mark: new RegExp(),
   unmarkOptions: {},
 }
 
