ef2d3cde96ceea31df8e0d699e3670a5e262e2bc
Aakash Shah <subs@appsparkler.com>
2021-02-10 06:32:08 +0530

    Updated PM2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     // react-mark.js Storybook
     {
-      script: 'yarn --cwd packages/react-mark.js storybook',
+      script: 'yarn workspace react-mark.js storybook',
       name: 'react-markjs-sb : 2001',
       env: {
         NODE_ENV: 'development',
@@ -17,22 +17,22 @@ module.exports = {
 
     // My Storybook
     {
-      script: 'yarn --cwd packages/my-storybook storybook',
+      script: 'yarn workspace my-storybook storybook',
       name: 'my-sb : 2000',
     },
 
     // HFN Gatsby Storybook
-    // {
-    //   script: 'yarn workspace react-hfn-gatsby-components storybook',
-    //   name: 'hfn-gatsby',
-    // },
+    {
+      script: 'yarn workspace react-hfn-gatsby-components storybook',
+      name: 'hfn-gatsby : 2002',
+    },
     // React HFN Components Storybook
-    // {
-    // script: 'yarn workspace @appsparkler/react-hfn-components storybook',
-    // name: 'react-hfn-components',
-    // env: {
-    //   NODE_ENV: 'development',
-    // },
-    // },
+    {
+      script: 'yarn workspace @appsparkler/react-hfn-components storybook',
+      name: 'react-hfn-components | 2003',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
   ],
 }
