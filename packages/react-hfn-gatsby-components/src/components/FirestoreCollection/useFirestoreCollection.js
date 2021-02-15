d2f9688b6b5c0146413be31b6163b3aa90b3cd4b
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-15 10:45:39 +0530

    Throwing error if path-segments is events

--- packages/react-hfn-gatsby-components/src/components/FirestoreCollection/useFirestoreCollection.js
+++ packages/react-hfn-gatsby-components/src/components/FirestoreCollection/useFirestoreCollection.js
@@ -2,6 +2,11 @@ import { useSelector } from 'react-redux'
 import { useFirestoreConnect } from 'react-redux-firebase'
 
 const useFirestoreCollection = (collectionPath) => {
+  const numberOfSegments = collectionPath.split('/').filter((i) => i).length
+  const isEven = numberOfSegments % 2 === 0
+  if (isEven) {
+    throw new Error('Collection path segments should be odd!')
+  }
   useFirestoreConnect(() => [{ collection: collectionPath }])
   const selector = ({ firestore: { data } }) => data[collectionPath]
   const collection = useSelector(selector)
