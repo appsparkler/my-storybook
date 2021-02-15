5979e6c6755c1baede222c6c42ce3d5cfdb71c7f
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-15 15:20:41 +0530

    Moving hook to separate file

--- packages/react-hfn-gatsby-components/src/components/FileDownloader/main.stories.js
+++ packages/react-hfn-gatsby-components/src/components/FileDownloader/main.stories.js
@@ -1,35 +1,5 @@
 import React from 'react'
-import { useFirebase } from 'react-redux-firebase'
-const useFileDownloader = () => {
-  const firebase = useFirebase()
-  const [{ isDownloading }, setState] = React.useState({
-    isDownloading: false,
-  })
-  const downloadFile = React.useCallback(
-    async (fullPath) => {
-      setState((currentState) => ({
-        ...currentState,
-        isDownloading: true,
-      }))
-      const storageRef = firebase.storage().ref(fullPath)
-      const downloadUrl = await storageRef.getDownloadURL()
-      Object.assign(document.createElement('a'), {
-        target: '_blank',
-        href: downloadUrl,
-      }).click()
-      setState((currentState) => ({
-        ...currentState,
-        isDownloading: false,
-      }))
-    },
-    [firebase]
-  )
-
-  return {
-    isDownloading,
-    downloadFile,
-  }
-}
+import useFileDownloader from './useFileDownloader'
 
 const FileDownloader = ({ filePath }) => {
   const { downloadFile, isDownloading } = useFileDownloader()
