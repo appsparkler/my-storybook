8c828893632ded8dc8d4230e5a12ebd73876ba35
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 11:31:05 +0530

    exporting useFileManager from index file

--- packages/react-hfn-gatsby-components/src/index.js
+++ packages/react-hfn-gatsby-components/src/index.js
@@ -2,10 +2,12 @@ import useFileUploader from './components/FileUploader/useFileUploader'
 import useFileDownloader from './components/FileDownloader/useFileDownloader'
 import useFileRemover from './components/FileRemover/useFileRemover'
 import useFirestoreCollection from './components/FirestoreCollection/useFirestoreCollection'
+import useFileManager from './components/FileManager/useFileManager'
 
 export {
   useFileUploader,
   useFileDownloader,
   useFileRemover,
   useFirestoreCollection,
+  useFileManager,
 }
