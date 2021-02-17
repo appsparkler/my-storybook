668b3066a32f43f855684f5ce8d101f31e6e18e9
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 06:40:55 +0530

    Adding onError callback for catching errors during download

--- packages/react-hfn-gatsby-components/src/components/FileDownloader/main.stories.js
+++ packages/react-hfn-gatsby-components/src/components/FileDownloader/main.stories.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import FileDownloader from '.'
+import useFileDownloader from './useFileDownloader'
 
 const Story = {
   title: 'Components/File Downloader',
-  component: FileDownloader,
 }
 
 export default Story
 
-const Template = (args) => <FileDownloader {...args} />
+const Template = ({ filePath }) => {
+  const { downloadFile, downloadingFileList } = useFileDownloader(console.error)
+  const onClick = React.useCallback(async () => {
+    await downloadFile(filePath)
+  }, [downloadFile, filePath])
+  return (
+    <div>
+      <button onClick={onClick} disabled={downloadingFileList.length}>
+        {downloadingFileList.length
+          ? `Downloading ${downloadingFileList.length} file...`
+          : 'Open/Download File'}
+      </button>
+      <pre>{JSON.stringify({ downloadingFileList }, null, 2)}</pre>
+    </div>
+  )
+}
 Template.args = {
   filePath: 'uploadedFiles/522d2ac7-8e30-4c48-8747-41ca37bb76d7-env-with-auth',
 }
--- packages/react-hfn-gatsby-components/src/components/FileDownloader/useFileDownloader.js
+++ packages/react-hfn-gatsby-components/src/components/FileDownloader/useFileDownloader.js
@@ -1,37 +1,42 @@
 import React from 'react'
 import { useFirebase } from 'react-redux-firebase'
 
-const useFileDownloader = () => {
+const useFileDownloader = (onError = () => null) => {
   const firebase = useFirebase()
   const [{ downloadingFileList }, setState] = React.useState({
     downloadingFileList: [],
   })
   const downloadFile = React.useCallback(
     async (fullPath) => {
-      setState((currentState) => ({
-        ...currentState,
-        downloadingFileList: (() => {
-          const updatedList = [...currentState.downloadingFileList]
-          updatedList.push(fullPath)
-          return updatedList
-        })(),
-      }))
-      const storageRef = firebase.storage().ref(fullPath)
-      const downloadUrl = await storageRef.getDownloadURL()
-      Object.assign(document.createElement('a'), {
-        target: '_blank',
-        href: downloadUrl,
-      }).click()
-      setState((currentState) => ({
-        ...currentState,
-        downloadingFileList: [
-          ...currentState.downloadingFileList.filter(
-            (listFile) => listFile !== fullPath
-          ),
-        ],
-      }))
+      try {
+        setState((currentState) => ({
+          ...currentState,
+          downloadingFileList: (() => {
+            const updatedList = [...currentState.downloadingFileList]
+            updatedList.push(fullPath)
+            return updatedList
+          })(),
+        }))
+        const storageRef = firebase.storage().ref(fullPath)
+        const downloadUrl = await storageRef.getDownloadURL()
+        Object.assign(document.createElement('a'), {
+          target: '_blank',
+          href: downloadUrl,
+        }).click()
+      } catch (e) {
+        onError(e)
+      } finally {
+        setState((currentState) => ({
+          ...currentState,
+          downloadingFileList: [
+            ...currentState.downloadingFileList.filter(
+              (listFile) => listFile !== fullPath
+            ),
+          ],
+        }))
+      }
     },
-    [firebase]
+    [firebase, onError]
   )
 
   return {
