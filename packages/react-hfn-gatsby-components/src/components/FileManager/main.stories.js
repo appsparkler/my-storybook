a155b795637338e9a283f597bfbaa2f600946ab0
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 11:30:31 +0530

    moving useFileManager to a separate file

--- packages/react-hfn-gatsby-components/src/components/FileManager/main.stories.js
+++ packages/react-hfn-gatsby-components/src/components/FileManager/main.stories.js
@@ -1,57 +1,11 @@
 import React from 'react'
-import useFileUploader from '../FileUploader/useFileUploader'
-import useFirestoreCollection from '../FirestoreCollection/useFirestoreCollection'
-import useFileRemover from '../FileRemover/useFileRemover'
-import useFileDownloader from '../FileDownloader/useFileDownloader'
+import useFileManager from './useFileManager'
 
 const Story = {
   title: 'Hooks/File Manager/useFileManager',
 }
 
 export default Story
-
-const useFileManager = ({
-  collectionPath,
-  storagePath,
-  onCollectionPathError = () => null,
-  onUploadError = () => null,
-  onDownloadError = () => null,
-  onRemoveError = () => null,
-} = {}) => {
-  const { uploadFiles, uploadingFileList } = useFileUploader({
-    collectionPath,
-    storagePath,
-    onError: onUploadError,
-  })
-  const files = useFirestoreCollection({
-    collectionPath,
-    onError: onCollectionPathError,
-  })
-  const { downloadFile, downloadingFileList } = useFileDownloader(
-    onDownloadError
-  )
-
-  const { removeFile, removingFileList } = useFileRemover({
-    onError: onRemoveError,
-  })
-
-  return {
-    // list of files in database
-    files,
-
-    // uploading files
-    uploadingFileList,
-    uploadFiles,
-
-    // downloading files
-    downloadFile,
-    downloadingFileList,
-
-    // removing files
-    removeFile,
-    removingFileList,
-  }
-}
 
 const Template = ({ collectionPath, storagePath }) => {
   const {
