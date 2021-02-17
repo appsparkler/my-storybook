dd2ea3a2d30710282bca716200881011a9f570f9
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 06:43:41 +0530

    useFileUploader

--- packages/react-hfn-gatsby-components/src/components/FileUploader/main.stories.js
+++ packages/react-hfn-gatsby-components/src/components/FileUploader/main.stories.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import FileUploader from '.'
+import useFileUploader from './useFileUploader'
 
 const Story = {
   title: 'Components/File Uploader',
-  component: FileUploader,
 }
 
 export default Story
 
-const Template = (args) => <FileUploader {...args} />
+const Template = ({
+  storagePath = 'uploadedFiles',
+  onError = () => null,
+  collectionPath = 'unnamed-collection',
+}) => {
+  const { uploadFiles, isUploading } = useFileUploader({
+    onError: (err) => console.error(err),
+    storagePath,
+    collectionPath,
+  })
+  const onChange = React.useCallback(
+    (evt) => {
+      const { files } = evt.target
+      uploadFiles(files)
+    },
+    [uploadFiles]
+  )
+  return (
+    <div>
+      <input type="file" onChange={onChange} />
+      {isUploading && 'Uploading...'}
+    </div>
+  )
+}
 Template.args = {
   storagePath: 'new-storage-path/abcd-xyz',
   collectionPath: 'my-super-storage-files',
