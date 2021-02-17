41c17a7f49821711f2df33314fd3fee00df2f60a
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 05:31:49 +0530

    Adding files being downloaded to downloadedFileList

--- packages/react-hfn-gatsby-components/src/components/FileDownloader-V2/index.js
+++ packages/react-hfn-gatsby-components/src/components/FileDownloader-V2/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import useFileDownloader from './useFileDownloader'
 
 const FileDownloader = ({ filePath }) => {
-  const { downloadFile, isDownloading } = useFileDownloader()
+  const { downloadFile, downloadingFileList } = useFileDownloader()
   const onClick = React.useCallback(async () => {
     await downloadFile(filePath)
   }, [downloadFile, filePath])
   return (
-    <button onClick={onClick} disabled={isDownloading}>
-      {isDownloading ? 'Downloading...' : 'Open/Download File'}
+    <button onClick={onClick} disabled={downloadingFileList.length}>
+      {downloadingFileList.length
+        ? `Downloading ${downloadingFileList.length} file...`
+        : 'Open/Download File'}
     </button>
   )
 }
