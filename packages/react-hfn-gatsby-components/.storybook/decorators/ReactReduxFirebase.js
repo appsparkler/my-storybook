328f5676ba6dc3dad10d17374b445e71208732c5
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 13:40:07 +0530

    Clean up

--- packages/react-hfn-gatsby-components/.storybook/decorators/ReactReduxFirebase.js
+++ packages/react-hfn-gatsby-components/.storybook/decorators/ReactReduxFirebase.js
@@ -3,50 +3,30 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore' // <- needed if using firestore
-// import 'firebase/functions' // <- needed if using httpsCallable
+import 'firebase/firestore'
 import 'firebase/storage'
-
-import {
-  ReactReduxFirebaseProvider,
-  // firebaseReducer,
-} from 'react-redux-firebase'
-import { createFirestoreInstance } from 'redux-firestore' // <- needed if using firestore
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
 import store from './store'
 
 const fbConfig = JSON.parse(process.env.STORYBOOK_FIREBASE_CONFIG)
 
 // react-redux-firebase config
 const rrfConfig = {
-  userProfile: 'users',
   useFirestoreForStorageMeta: true,
-  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
-  // enableClaims: true // Get custom claims along with the profile
 }
 
 // Initialize firebase instance
 firebase.initializeApp(fbConfig)
 
 // Initialize other services on firebase instance
-firebase.firestore() // <- needed if using firestore
-// firebase.functions() // <- needed if using httpsCallable
+firebase.firestore()
 
-// Add firebase to reducers
-// const rootReducer = combineReducers({
-//   firebase: firebaseReducer,
-//   firestore: firestoreReducer, // <- needed if using firestore
-// })
-
-// Create store with reducers and initial state
-const initialState = {
-  data: {},
-}
 const rrfProps = {
   firebase,
   config: rrfConfig,
   dispatch: store.dispatch,
-  createFirestoreInstance, // <- needed if using firestore
+  createFirestoreInstance,
 }
 
 export default (Story) => (
