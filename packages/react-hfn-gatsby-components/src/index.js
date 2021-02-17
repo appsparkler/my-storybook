50fe8dea57ee8ea35586ada9a7a1945a9650ba60
Aakash Shah <aakash.shah@publicissapient.com>
2021-02-17 05:50:36 +0530

    Exporting the hooks from index file

--- packages/react-hfn-gatsby-components/src/index.js
+++ packages/react-hfn-gatsby-components/src/index.js
@@ -1,4 +1,11 @@
-import useFileUpload from './components/FileUpload/useFileUpload'
-import useFileDownloader from './components/FileUpload/useFileDownloader'
+import useFileUploader from './components/FileUploader/useFileUploader'
+import useFileDownloader from './components/FileDownloader/useFileDownloader'
+import useFileRemover from './components/FileRemover/useFileRemover'
+import useFirestoreCollection from './components/FirestoreCollection/useFirestoreCollection'
 
-export { useFileUpload, useFileDownloader }
+export {
+  useFileUploader,
+  useFileDownloader,
+  useFileRemover,
+  useFirestoreCollection,
+}
