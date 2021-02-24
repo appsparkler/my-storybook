d0a6312a9f8d07d19836d9b782df7ef7eccb53aa
Aakash Shah <appsparkler@gmail.com>
2021-02-24 13:59:43 +0530

    Added a list of items story

--- packages/my-storybook/src/components/ListItem/__stories__/main.stories.js
+++ packages/my-storybook/src/components/ListItem/__stories__/main.stories.js
@@ -13,6 +13,30 @@ Template.args = {
 export const Example = Template.bind({})
 Example.args = { ...Template.args }
 
+const MultipleItems = ({ items }) =>
+  Array.isArray(items)
+    ? items.map((item) => <ListItem key={item.id} {...item} />)
+    : null
+
+export const ListOfItems = () => (
+  <MultipleItems
+    items={[
+      {
+        id: '1',
+        isLast: false,
+        mainText: 'Asia/Kolkata',
+        subText: 'IN',
+      },
+      {
+        id: '2',
+        isLast: true,
+        mainText: 'Asia/Hong Kong',
+        subText: 'HK',
+      },
+    ]}
+  />
+)
+
 const Story = {
   title: 'Components/List Item',
   component: ListItem,
