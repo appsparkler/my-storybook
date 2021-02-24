7ea7525a8f399ac7bb0593a71a36945c8cbc5e0a
Aakash Shah <appsparkler@gmail.com>
2021-02-24 16:53:11 +0530

    ListItems story

--- packages/my-storybook/src/components/ListItem/__stories__/main.stories.js
+++ packages/my-storybook/src/components/ListItem/__stories__/main.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Stack, mergeStyleSets } from '@fluentui/react'
 import ListItem from '..'
 
 const Template = (args) => {
@@ -13,10 +14,40 @@ Template.args = {
 export const Example = Template.bind({})
 Example.args = { ...Template.args }
 
-const MultipleItems = ({ items }) =>
-  Array.isArray(items)
-    ? items.map((item) => <ListItem key={item.id} {...item} />)
-    : null
+const ListItems = ({ items }) =>
+  items.map((item) => <ListItem key={item.id} {...item} />)
+
+const MultipleItems = ({ items }) => {
+  const styles = React.useMemo(
+    () =>
+      mergeStyleSets({
+        wrapper: {
+          border: 'thick lightgreen ridge',
+          maxHeight: 300,
+          overflow: 'auto',
+          mark: {
+            borderBottomLeftRadius: 8,
+            borderBottomRightRadius: 3,
+            borderTopRightRadius: 8,
+            borderTopLeftRadius: 3,
+            padding: 1,
+            background: 'lightgreen',
+            opacity: 0.87,
+            textShadow: '0px 0px 2px rgba(0,0,0,.6)',
+            color: 'black',
+          },
+        },
+      }),
+    []
+  )
+  console.log(Array.isArray(items))
+  if (!Array.isArray(items)) return null
+  return (
+    <Stack className={styles.wrapper}>
+      <ListItems items={items} />
+    </Stack>
+  )
+}
 
 export const ListOfItems = () => (
   <MultipleItems
