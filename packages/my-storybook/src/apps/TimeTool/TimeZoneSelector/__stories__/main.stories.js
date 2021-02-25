f235ed1277772bde571f652a61d86da47217e0b4
Aakash Shah <appsparkler@gmail.com>
2021-02-25 13:45:56 +0530

    Setting up region-dropdown

--- packages/my-storybook/src/apps/TimeTool/TimeZoneSelector/__stories__/main.stories.js
+++ packages/my-storybook/src/apps/TimeTool/TimeZoneSelector/__stories__/main.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Stack, Dropdown, Callout, TextField } from '@fluentui/react'
 import TimeZoneSelector from '../index.jsx'
 
 const Story = {
@@ -8,5 +9,46 @@ const Story = {
 
 export const Example = (args) => <TimeZoneSelector {...args} />
 Example.args = {}
+
+const useTimezoneDropdown = () => {
+  return {
+    onSelectTimezone: React.useCallback(() => {}, []),
+  }
+}
+
+const RegionDropdown = ({ onSelectRegion }) => {
+  const timezoneDropdown = React.useMemo(() => {
+    return {
+      placeholder: 'Select Region',
+      options: [
+        {
+          key: 'Americas',
+          text: 'Americas',
+        },
+        {
+          key: 'Asia',
+          text: 'Asia',
+        },
+      ],
+      onChange: (evt, selectedRegion) => {
+        onSelectRegion(selectedRegion)
+      },
+    }
+  }, [onSelectRegion])
+  return <Dropdown {...timezoneDropdown} />
+}
+
+export const ExampleV2 = () => {
+  const onSelectRegion = React.useCallback((tz) => {}, [])
+  return (
+    <Stack>
+      <RegionDropdown onSelectRegion={onSelectRegion} />
+      {/*<TextField  />
+      <Callout >
+        <h1>Hey A Callout!</h1>
+      </Callout>*/}
+    </Stack>
+  )
+}
 
 export default Story
