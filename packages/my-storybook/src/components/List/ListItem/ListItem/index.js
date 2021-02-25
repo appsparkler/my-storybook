5e1a6252cd83b262ef7934267c6b8966f8913f60
Aakash Shah <appsparkler@gmail.com>
2021-02-25 10:35:07 +0530

    Setting up list item

--- packages/my-storybook/src/components/List/ListItem/ListItem/index.js
+++ packages/my-storybook/src/components/List/ListItem/ListItem/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { mergeStyleSets, Stack, Text, getTheme } from '@fluentui/react'
 
-const ListItem = ({ isLast, mainText, subText, onClick }) => {
+const ListItem = ({ id, isLast, mainText, subText, onClick }) => {
   const theme = getTheme()
+
   const styles = React.useMemo(
     () =>
       mergeStyleSets({
         wrapper: {
           padding: 2,
-          borderTop: 'thin black solid',
+          borderTop: `thin ${theme.palette.themeDarker} solid`,
           borderBottom: !isLast ? 0 : `thin ${theme.palette.themeDarker} solid`,
           cursor: 'pointer',
           button: {
@@ -20,9 +21,18 @@ const ListItem = ({ isLast, mainText, subText, onClick }) => {
       }),
     [isLast, theme]
   )
+
+  const handleClick = React.useCallback(
+    (...args) => {
+      const isOnClickFunction = typeof onClick === 'function'
+      if (isOnClickFunction) onClick({ id, isLast, mainText, subText }, ...args)
+    },
+    [id, isLast, mainText, subText, onClick]
+  )
+
   return (
     <Stack vertical className={styles.wrapper}>
-      <button onClick={onClick}>
+      <button onClick={handleClick}>
         <Stack vertical>
           <Text variant="large">{mainText}</Text>
           <Text variant="small">{subText}</Text>
--- packages/my-storybook/src/components/List/ListItem/index.js
+++ packages/my-storybook/src/components/List/ListItem/index.js
@@ -1,49 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { mergeStyleSets, Stack, Text, getTheme } from '@fluentui/react'
+import ListItem from './ListItem'
 
-const ListItem = ({ isLast, mainText, subText, onClick }) => {
-  const theme = getTheme()
-  const styles = React.useMemo(
-    () =>
-      mergeStyleSets({
-        wrapper: {
-          padding: 2,
-          borderTop: 'thin black solid',
-          borderBottom: !isLast ? 0 : `thin ${theme.palette.themeDarker} solid`,
-          cursor: 'pointer',
-          button: {
-            border: 0,
-            textAlign: 'left',
-          },
-        },
-      }),
-    [isLast, theme]
-  )
-  return (
-    <Stack vertical className={styles.wrapper}>
-      <button onClick={onClick}>
-        <Stack vertical>
-          <Text variant="large">{mainText}</Text>
-          <Text variant="small">{subText}</Text>
-        </Stack>
-      </button>
-    </Stack>
-  )
-}
-
-ListItem.propTypes = {
-  mainText: PropTypes.string,
-  subText: PropTypes.string,
-  onClick: PropTypes.func,
-  isLast: PropTypes.bool,
-}
-
-const ListItems = ({ items, onSelectItem }) => {
-  return items.map((item) => (
+const ListItems = ({ items, onSelectItem }) =>
+  items.map((item) => (
     <ListItem key={item.id} {...item} onClick={onSelectItem} />
   ))
-}
 
 ListItems.propTypes = {
   items: PropTypes.arrayOf(
